7dc44772ccacf7fec6d0abed635bbef99ed9266a
Ankit <mitit.ankit@gmail.com>
2016-01-10 21:23:27 +0530

    New change

--- ePaper/WebContent/js/loginjs.js
+++ ePaper/WebContent/js/loginjs.js
@@ -1,27 +1,7 @@
 function teacherLogin(){
-//	$("loginButtons").css("visibility", "hidden");
-	var url1 = "SignIn.jsp";
-	 
-    $.ajax({
-        url: url1,
-        context: document.body,
-        success: function(html){                         
-            $("#showLogin").html(html)
-           // alert(showLogin);
-        }
-    });
+	window.location = "RegistrationFaculty.html";
 }
 
 function studentLogin(){
-//	$("loginButtons").css("visibility", "hidden");
-	var url1 = "SignIn.jsp";
-	 
-    $.ajax({
-        url: url1,
-        context: document.body,
-        success: function(html){                         
-            $("#showLogin").html(html)
-            //alert(showLogin);
-        }
-    });
+	window.location = "RegistrationStudent.html"
 }
