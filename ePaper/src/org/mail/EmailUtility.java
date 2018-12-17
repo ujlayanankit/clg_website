c52868f8770a14957fc2c8b0f8c86abb9a36d461
Ankit Verma <mitit.ankit@gmail.com>
2018-12-17 13:36:57 +0530

    some change

--- ePaper/src/org/mail/EmailUtility.java
+++ ePaper/src/org/mail/EmailUtility.java
@@ -21,8 +21,8 @@ public class EmailUtility {
 		Properties props = new Properties();
 	      props.put("mail.smtp.auth", "true");
 	      props.put("mail.smtp.starttls.enable", "true");
-	      props.put("mail.smtp.host", "smtp.gmail.com");
-	      props.put("mail.smtp.port", 587);
+	      props.put("mail.smtp.host", "smtp.XXX.com");
+	      props.put("mail.smtp.port", XXX);
 
 		// creates a new session with an authenticator
 		/* Authenticator auth = new Authenticator() {
